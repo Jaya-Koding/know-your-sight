59aa3a7f0c3d37b744c82a063bf5eb7a34529868
Jimly Assidqi <jimlyasidqi692@gmail.com>
2024-12-08 19:23:06 +0700

    Update price

--- src/pages/UpgradePlan.tsx
+++ src/pages/UpgradePlan.tsx
@@ -6,33 +6,38 @@ const UpgradePlan:React.FC = () => {
     <div className='h-screen flex items-center justify-center'>
         <div className='max-w-[1080px] py-24'>
             <h4 className='text-center text-2xl font-medium'>Upgrade Plan</h4>
-            <div className="grid sm:grid-cols-2 py-10 px-24 gap-3">
-                <div className='border p-7 rounded-lg'>
-                    <h4 className='text-2xl font-medium'>Free</h4>
+            <div className="grid sm:grid-cols-3 py-10 px-24 gap-10">
+                <div className='border p-7 rounded-lg w-[300px]'>
+                    <h4 className='text-2xl font-medium'>Starter</h4>
                     <div className='my-5'>
-                        <p><span className='text-4xl'>Rp0</span> / month</p>
-                        <p className='text-sm mt-3'>Explore basic eye health detection features.</p>
+                        <p><span className='text-2xl'>Rp200.000</span></p>
                     </div>
-                    <p className='my-5 p-3 rounded-full text-sm text-center text-slate-400 bg-gray-100'>Current plan</p>
+                    <p className='my-5 p-3 rounded-full text-sm text-center text-white bg-aksen cursor-pointer'>Upgrade</p>
                     <ul className='text-[13px] font-[400]'>
-                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> Assistance with basic eye health screening</li>
-                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> Access to limited AI disease detection</li>
-                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> View basic diagnostic suggestions</li>
-                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> Learn about common eye conditions</li>
+                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> 5 Prediction</li>
+                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> 5 Print Report</li>
                     </ul>
                 </div>
-                <div className='border p-7 rounded-lg'>
-                    <h4 className='text-2xl font-medium'>Plus</h4>
+                <div className='border p-7 rounded-lg w-[300px]'>
+                    <h4 className='text-2xl font-medium'>Basic</h4>
                     <div className='my-5'>
-                        <p><span className='text-4xl'>Rp150.000</span> / month</p>
-                        <p className='text-sm mt-3'>Unlock advanced AI tools for eye health analysis.</p>
+                        <p><span className='text-2xl'>Rp550.000</span></p>
                     </div>
-                    <p className='my-5 p-3 rounded-full text-sm text-center text-white bg-aksen'>Upgrade</p>
+                    <p className='my-5 p-3 rounded-full text-sm text-center text-white bg-aksen cursor-pointer'>Upgrade</p>
                     <ul className='text-[13px] font-[400]'>
-                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> Comprehensive eye disease detection</li>
-                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> Access to advanced AI for rare conditions</li>
-                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> Personalized diagnostic reports</li>
-                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> Recommendations for treatment and specialists</li>
+                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> 15 Prediction</li>
+                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> 15 Print Report</li>
+                    </ul>
+                </div>
+                <div className='border p-7 rounded-lg w-[300px]'>
+                    <h4 className='text-2xl font-medium'>Premium</h4>
+                    <div className='my-5'>
+                        <p><span className='text-2xl'>Rp900.000</span></p>
+                    </div>
+                    <p className='my-5 p-3 rounded-full text-sm text-center text-slate-400 bg-gray-100'>Current Plan</p>
+                    <ul className='text-[13px] font-[400]'>
+                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> 30 Prediction</li>
+                        <li className='flex items-center gap-x-2 mb-3'><FaCheck /> 30 Print Report</li>
                     </ul>
                 </div>
             </div>
